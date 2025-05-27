99afba14878a90f5b25de0f97fc013c3ae036aa3
khoiYeuMe <133907774+hoaikhoitran@users.noreply.github.com>
2025-05-27 14:22:51 +0700

    second

--- DrugPreventionSystemBE/obj/Debug/net8.0/DrugPreventionSystemBE.AssemblyInfo.cs
+++ DrugPreventionSystemBE/obj/Debug/net8.0/DrugPreventionSystemBE.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DrugPreventionSystemBE")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+46d39f87111d4152da1ec8270727282e3acc0ff1")]
 [assembly: System.Reflection.AssemblyProductAttribute("DrugPreventionSystemBE")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DrugPreventionSystemBE")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
