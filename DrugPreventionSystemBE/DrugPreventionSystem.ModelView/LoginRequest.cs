69218be7736f6a106c0b2b569720332c0f75c280
colgnaoh <lochoang2303@gmail.comgit config --global user.email lochoang2303@gmail.com>
2025-05-30 14:31:33 +0700

    remove .vs folder

---
+++ DrugPreventionSystemBE/DrugPreventionSystem.ModelView/LoginRequest.cs
@@ -0,0 +1,8 @@
+﻿namespace DrugPreventionSystemBE.DrugPreventionSystem.ModelView
+{
+    public class LoginRequest
+    {
+        public string Email { get; set; }
+        public string Password { get; set; }
+    }
+}
