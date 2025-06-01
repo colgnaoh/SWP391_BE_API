02277045784e6a641e7b7d3e8b4e5362becad5d7
khoiYeuMe <133907774+hoaikhoitran@users.noreply.github.com>
2025-06-01 23:04:57 +0700

    deployWithAzure

---
+++ DrugPreventionSystemBE/DrugPreventionSystem.Controller/DeployTest.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DrugPreventionSystemBE.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TestController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get() => Ok("API Working!");
+    }
+}
+//https://deploy-webapi-gvashjepf9hnc8bp.australiaeast-01.azurewebsites.net/api/test
