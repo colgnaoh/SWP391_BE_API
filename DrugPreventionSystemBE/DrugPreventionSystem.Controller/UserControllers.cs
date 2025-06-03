5a7151e2faa65fe65212758fe0800104e622fa50
colgnaoh <lochoang2303@gmail.comgit config --global user.email lochoang2303@gmail.com>
2025-06-03 13:47:22 +0700

    user CRUD

--- DrugPreventionSystemBE/DrugPreventionSystem.Controller/UserControllers.cs
+++ DrugPreventionSystemBE/DrugPreventionSystem.Controller/UserControllers.cs
@@ -1,24 +1,115 @@
 ﻿using DrugPreventionSystemBE.DrugPreventionSystem.Data;
 using DrugPreventionSystemBE.DrugPreventionSystem.Enity;
-using DrugPreventionSystemBE.DrugPreventionSystem.ModelView;
-using DrugPreventionSystemBE.DrugPreventionSystem.Service;
-using Microsoft.EntityFrameworkCore;
-using DrugPreventionSystemBE.DrugPreventionSystem.Service;
-
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Configuration;
-
-using System.Data.SqlClient;
-using DrugPreventionSystemBE.DrugPreventionSystem.Helpers;
-using DrugPreventionSystemBE.DrugPreventionSystem.Enum;
-
+using Microsoft.EntityFrameworkCore;
+using System;
 
 namespace DrugPreventionSystemBE.DrugPreventionSystem.Controller
 {
+    [Route("api/[controller]")]
     [ApiController]
-    [Route("/api/user")]
-    public class UserControllers : ControllerBase
+    public class UsersController : ControllerBase
     {
+        private readonly DrugPreventionDbContext _context;
 
+        public UsersController(DrugPreventionDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Users
+        [HttpGet]
+        [Authorize(Roles = "Admin")]
+        public async Task<ActionResult<IEnumerable<User>>> GetUsers()
+        {
+            return await _context.Users.ToListAsync();
+        }
+
+        // GET: api/Users/{id}
+        [HttpGet("{id}")]
+        [Authorize(Roles = "Admin")]
+        public async Task<ActionResult<User>> GetUser(Guid id)
+        {
+            var user = await _context.Users.FindAsync(id);
+
+            if (user == null)
+            {
+                return NotFound();
+            }
+
+            return user;
+        }
+
+        // PUT: api/Users/{id}
+        [HttpPut("{id}")]
+        [Authorize(Roles = "Admin")]
+        public async Task<IActionResult> UpdateUser(Guid id, User updatedUser)
+        {
+            if (id != updatedUser.Id)
+            {
+                return BadRequest();
+            }
+
+            var user = await _context.Users.FindAsync(id);
+            if (user == null)
+            {
+                return NotFound();
+            }
+
+            user.FirstName = updatedUser.FirstName;
+            user.LastName = updatedUser.LastName;
+            user.Email = updatedUser.Email;
+            user.PhoneNumber = updatedUser.PhoneNumber;
+            user.Address = updatedUser.Address;
+            user.Gender = updatedUser.Gender;
+            user.Dob = updatedUser.Dob;
+            user.Role = updatedUser.Role;
+            user.AgeGroup = updatedUser.AgeGroup;
+            user.IsVerified = updatedUser.IsVerified;
+            user.UpdatedAt = DateTime.UtcNow;
+
+            _context.Entry(user).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!UserExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // DELETE: api/Users/{id}
+        [HttpDelete("{id}")]
+        [Authorize(Roles = "Admin")]
+        public async Task<IActionResult> DeleteUser(Guid id)
+        {
+            var user = await _context.Users.FindAsync(id);
+            if (user == null)
+            {
+                return NotFound();
+            }
+
+            _context.Users.Remove(user);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool UserExists(Guid id)
+        {
+            return _context.Users.Any(e => e.Id == id);
+        }
     }
 }
