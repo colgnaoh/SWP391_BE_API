8a0beec59ec402f7f4be1993b6c92c162237dfde
colgnaoh <lochoang2303@gmail.comgit config --global user.email lochoang2303@gmail.com>
2025-06-06 12:43:40 +0700

    updateUserController

--- DrugPreventionSystemBE/DrugPreventionSystem.Service/Interface/IUserServices.cs
+++ DrugPreventionSystemBE/DrugPreventionSystem.Service/Interface/IUserServices.cs
@@ -1,6 +1,16 @@
-﻿namespace DrugPreventionSystemBE.DrugPreventionSystem.Service.Interface
+﻿using DrugPreventionSystemBE.DrugPreventionSystem.Enity;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace DrugPreventionSystemBE.DrugPreventionSystem.Services
 {
-    public class IUserServices
+    public interface IUserService
     {
+        Task<IEnumerable<User>> GetAllUsersAsync();
+        Task<User?> GetUserByIdAsync(Guid id);
+        Task<bool> UpdateUserAsync(Guid id, User updatedUser);
+        Task<bool> DeleteUserAsync(Guid id);
+        bool UserExists(Guid id);
     }
 }
---
+++ DrugPreventionSystemBE/DrugPreventionSystem.Service/UserService.cs
@@ -0,0 +1,82 @@
+﻿using DrugPreventionSystemBE.DrugPreventionSystem.Data;
+using DrugPreventionSystemBE.DrugPreventionSystem.Enity;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace DrugPreventionSystemBE.DrugPreventionSystem.Services
+{
+    public class UserService : IUserService
+    {
+        private readonly DrugPreventionDbContext _context;
+
+        public UserService(DrugPreventionDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IEnumerable<User>> GetAllUsersAsync()
+        {
+            return await _context.Users.ToListAsync();
+        }
+
+        public async Task<User?> GetUserByIdAsync(Guid id)
+        {
+            return await _context.Users.FindAsync(id);
+        }
+
+        public async Task<bool> UpdateUserAsync(Guid id, User updatedUser)
+        {
+            if (id != updatedUser.Id)
+                return false;
+
+            var user = await _context.Users.FindAsync(id);
+            if (user == null)
+                return false;
+
+            user.FirstName = updatedUser.FirstName;
+            user.LastName = updatedUser.LastName;
+            user.Email = updatedUser.Email;
+            user.PhoneNumber = updatedUser.PhoneNumber;
+            user.Address = updatedUser.Address;
+            user.Gender = updatedUser.Gender;
+            user.Dob = updatedUser.Dob;
+            user.Role = updatedUser.Role;
+            user.AgeGroup = updatedUser.AgeGroup;
+            user.IsVerified = updatedUser.IsVerified;
+            user.UpdatedAt = DateTime.UtcNow;
+
+            _context.Entry(user).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+                return true;
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!UserExists(id))
+                    return false;
+
+                throw;
+            }
+        }
+
+        public async Task<bool> DeleteUserAsync(Guid id)
+        {
+            var user = await _context.Users.FindAsync(id);
+            if (user == null)
+                return false;
+
+            _context.Users.Remove(user);
+            await _context.SaveChangesAsync();
+            return true;
+        }
+
+        public bool UserExists(Guid id)
+        {
+            return _context.Users.Any(u => u.Id == id);
+        }
+    }
+}
