2473226b5e6c96551748cc1b756505dc72275a8f
colgnaoh <lochoang2303@gmail.comgit config --global user.email lochoang2303@gmail.com>
2025-06-09 13:00:32 +0700

    ProgramCRUDv2

---
+++ DrugPreventionSystemBE/DrugPreventionSystem.Controller/CommunityProgramController.cs
@@ -0,0 +1,58 @@
+﻿using DrugPreventionSystemBE.DrugPreventionSystem.Enity;
+using CommunityProgramEntity = DrugPreventionSystemBE.DrugPreventionSystem.Enity.CommunityProgram;
+using DrugPreventionSystemBE.DrugPreventionSystem.Entity;
+using DrugPreventionSystemBE.DrugPreventionSystem.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DrugPreventionSystemBE.DrugPreventionSystem.Controller
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PreventionProgramController : ControllerBase
+    {
+        private readonly ICommunityProgramService _programService;
+
+        public PreventionProgramController(ICommunityProgramService programService)
+        {
+            _programService = programService;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<CommunityProgram>>> GetPrograms()
+        {
+            var programs = await _programService.GetAllProgramsAsync();
+            return Ok(programs);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<CommunityProgram>> GetProgram(Guid id)
+        {
+            var program = await _programService.GetProgramByIdAsync(id);
+            if (program == null) return NotFound();
+            return Ok(program);
+        }
+
+        public async Task<ActionResult<CommunityProgram>> CreateCommunityProgramAsync(CommunityProgram program)
+        {
+            var created = await _programService.CreateCommunityProgramAsync(program);
+            return CreatedAtAction(nameof(GetProgram), new { id = created.Id }, program);
+        }
+
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> UpdateCommunityProgram(Guid id, CommunityProgram program)
+        {
+            var result = await _programService.UpdateCommunityProgramAsync(id, program);
+            if (!result) return NotFound();
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteProgram(Guid id)
+        {
+            var result = await _programService.DeleteProgramAsync(id);
+            if (!result) return NotFound();
+            return NoContent();
+        }
+    }
+}
--- DrugPreventionSystemBE/DrugPreventionSystem.Service/Interface/ICommunityProgramService.cs
+++ DrugPreventionSystemBE/DrugPreventionSystem.Service/Interface/ICommunityProgramService.cs
@@ -4,10 +4,10 @@ namespace DrugPreventionSystemBE.DrugPreventionSystem.Services
 {
     public interface ICommunityProgramService
     {
-        Task<IEnumerable<Program>> GetAllProgramsAsync();
-        Task<Program?> GetProgramByIdAsync(Guid id);
-        Task<Program> CreateProgramAsync(Program program);
-        Task<bool> UpdateProgramAsync(Guid id, Program updatedProgram);
+        Task<IEnumerable<CommunityProgram>> GetAllProgramsAsync();
+        Task<CommunityProgram?> GetProgramByIdAsync(Guid id);
+        Task<CommunityProgram> CreateCommunityProgramAsync(CommunityProgram program);
+        Task<bool> UpdateCommunityProgramAsync(Guid id, CommunityProgram updatedProgram);
         Task<bool> DeleteProgramAsync(Guid id);
     }
 }
