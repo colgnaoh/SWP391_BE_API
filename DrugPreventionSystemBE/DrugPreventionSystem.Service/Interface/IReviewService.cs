bb43890dc647b5cabab252feafb6259fcf60e479
colgnaoh <lochoang2303@gmail.comgit config --global user.email lochoang2303@gmail.com>
2025-06-17 12:21:17 +0700

    ReviewCRUD

---
+++ DrugPreventionSystemBE/DrugPreventionSystem.Service/Interface/IReviewService.cs
@@ -0,0 +1,15 @@
+﻿using DrugPreventionSystemBE.DrugPreventionSystem.ModelView.ResponseModel;
+using DrugPreventionSystemBE.DrugPreventionSystem.ModelView.ReviewReqModel;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DrugPreventionSystemBE.DrugPreventionSystem.Service.Interface
+{
+    public interface IReviewService
+    {
+        Task<List<ReviewResModel>> GetAllReviewsAsync();
+        Task<ReviewResModel?> GetReviewByIdAsync(Guid id);
+        Task<ReviewResModel> CreateReviewAsync(CreateReviewReqModel request);
+        Task<bool> UpdateReviewAsync(Guid id, UpdateReviewReqModel request);
+        Task<bool> DeleteReviewAsync(Guid id);
+    }
+}
