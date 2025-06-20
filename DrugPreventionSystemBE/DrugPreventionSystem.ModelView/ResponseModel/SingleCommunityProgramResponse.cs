e9986874bdd8f79e661bab51b15e47ab4e1c0784
colgnaoh <lochoang2303@gmail.comgit config --global user.email lochoang2303@gmail.com>
2025-06-20 12:30:11 +0700

    comProgram

---
+++ DrugPreventionSystemBE/DrugPreventionSystem.ModelView/ResponseModel/SingleCommunityProgramResponse.cs
@@ -0,0 +1,10 @@
+﻿using DrugPreventionSystemBE.DrugPreventionSystem.ModelView.CommunityProgramResModel;
+
+namespace DrugPreventionSystemBE.DrugPreventionSystem.ModelView.ResponseModel
+{
+    public class SingleCommunityProgramResponse
+    {
+        public bool Success { get; set; }
+        public CommunityProgramResponseModel Data { get; set; }
+    }
+}
