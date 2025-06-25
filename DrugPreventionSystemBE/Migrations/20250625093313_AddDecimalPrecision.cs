bad52cb2f7c7ee872da9c499b1ce5920505e2cb1
khoiYeuMe <133907774+hoaikhoitran@users.noreply.github.com>
2025-06-25 16:33:51 +0700

    migrations add AddDecimalPrecision

---
+++ DrugPreventionSystemBE/Migrations/20250625093313_AddDecimalPrecision.cs
@@ -0,0 +1,278 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DrugPreventionSystemBE.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddDecimalPrecision : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Consultants_Users_UserId",
+                table: "Consultants");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Consultants",
+                table: "Consultants");
+
+            migrationBuilder.RenameTable(
+                name: "Consultants",
+                newName: "consultants");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Consultants_UserId",
+                table: "consultants",
+                newName: "IX_consultants_UserId");
+
+            migrationBuilder.AlterColumn<decimal>(
+                name: "TotalAmount",
+                table: "Orders",
+                type: "decimal(10,2)",
+                precision: 10,
+                scale: 2,
+                nullable: false,
+                oldClrType: typeof(decimal),
+                oldType: "decimal(18,2)");
+
+            migrationBuilder.AlterColumn<decimal>(
+                name: "Amount",
+                table: "OrderDetails",
+                type: "decimal(10,2)",
+                precision: 10,
+                scale: 2,
+                nullable: true,
+                oldClrType: typeof(decimal),
+                oldType: "decimal(18,2)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<decimal>(
+                name: "Price",
+                table: "Courses",
+                type: "decimal(10,2)",
+                precision: 10,
+                scale: 2,
+                nullable: true,
+                oldClrType: typeof(decimal),
+                oldType: "decimal(18,2)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<decimal>(
+                name: "Discount",
+                table: "Courses",
+                type: "decimal(10,2)",
+                precision: 10,
+                scale: 2,
+                nullable: true,
+                oldClrType: typeof(decimal),
+                oldType: "decimal(18,2)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<decimal>(
+                name: "Salary",
+                table: "consultants",
+                type: "decimal(10,2)",
+                precision: 10,
+                scale: 2,
+                nullable: true,
+                oldClrType: typeof(decimal),
+                oldType: "decimal(18,2)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Qualifications",
+                table: "consultants",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "FullName",
+                table: "consultants",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Email",
+                table: "consultants",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<decimal>(
+                name: "Price",
+                table: "Carts",
+                type: "decimal(10,2)",
+                precision: 10,
+                scale: 2,
+                nullable: false,
+                oldClrType: typeof(decimal),
+                oldType: "decimal(18,2)");
+
+            migrationBuilder.AlterColumn<decimal>(
+                name: "Discount",
+                table: "Carts",
+                type: "decimal(10,2)",
+                precision: 10,
+                scale: 2,
+                nullable: false,
+                oldClrType: typeof(decimal),
+                oldType: "decimal(18,2)");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_consultants",
+                table: "consultants",
+                column: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_consultants_Users_UserId",
+                table: "consultants",
+                column: "UserId",
+                principalTable: "Users",
+                principalColumn: "Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_consultants_Users_UserId",
+                table: "consultants");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_consultants",
+                table: "consultants");
+
+            migrationBuilder.RenameTable(
+                name: "consultants",
+                newName: "Consultants");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_consultants_UserId",
+                table: "Consultants",
+                newName: "IX_Consultants_UserId");
+
+            migrationBuilder.AlterColumn<decimal>(
+                name: "TotalAmount",
+                table: "Orders",
+                type: "decimal(18,2)",
+                nullable: false,
+                oldClrType: typeof(decimal),
+                oldType: "decimal(10,2)",
+                oldPrecision: 10,
+                oldScale: 2);
+
+            migrationBuilder.AlterColumn<decimal>(
+                name: "Amount",
+                table: "OrderDetails",
+                type: "decimal(18,2)",
+                nullable: true,
+                oldClrType: typeof(decimal),
+                oldType: "decimal(10,2)",
+                oldPrecision: 10,
+                oldScale: 2,
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<decimal>(
+                name: "Price",
+                table: "Courses",
+                type: "decimal(18,2)",
+                nullable: true,
+                oldClrType: typeof(decimal),
+                oldType: "decimal(10,2)",
+                oldPrecision: 10,
+                oldScale: 2,
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<decimal>(
+                name: "Discount",
+                table: "Courses",
+                type: "decimal(18,2)",
+                nullable: true,
+                oldClrType: typeof(decimal),
+                oldType: "decimal(10,2)",
+                oldPrecision: 10,
+                oldScale: 2,
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<decimal>(
+                name: "Salary",
+                table: "Consultants",
+                type: "decimal(18,2)",
+                nullable: true,
+                oldClrType: typeof(decimal),
+                oldType: "decimal(10,2)",
+                oldPrecision: 10,
+                oldScale: 2,
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Qualifications",
+                table: "Consultants",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "FullName",
+                table: "Consultants",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Email",
+                table: "Consultants",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<decimal>(
+                name: "Price",
+                table: "Carts",
+                type: "decimal(18,2)",
+                nullable: false,
+                oldClrType: typeof(decimal),
+                oldType: "decimal(10,2)",
+                oldPrecision: 10,
+                oldScale: 2);
+
+            migrationBuilder.AlterColumn<decimal>(
+                name: "Discount",
+                table: "Carts",
+                type: "decimal(18,2)",
+                nullable: false,
+                oldClrType: typeof(decimal),
+                oldType: "decimal(10,2)",
+                oldPrecision: 10,
+                oldScale: 2);
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Consultants",
+                table: "Consultants",
+                column: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Consultants_Users_UserId",
+                table: "Consultants",
+                column: "UserId",
+                principalTable: "Users",
+                principalColumn: "Id");
+        }
+    }
+}
