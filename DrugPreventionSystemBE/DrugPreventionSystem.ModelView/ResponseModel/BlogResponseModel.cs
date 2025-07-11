7d97c0eb5062009042b1701b518e9f09e9f45b8b
colgnaoh <lochoang2303@gmail.comgit config --global user.email lochoang2303@gmail.com>
2025-07-12 00:51:52 +0700

    blog title

--- DrugPreventionSystemBE/DrugPreventionSystem.ModelView/ResponseModel/BlogResponseModel.cs
+++ DrugPreventionSystemBE/DrugPreventionSystem.ModelView/ResponseModel/BlogResponseModel.cs
@@ -4,6 +4,8 @@
     {
         public Guid Id { get; set; }
         public Guid UserId { get; set; }
+        public string? Title { get; set; }
+
         public string Content { get; set; }
         public string BlogImgUrl { get; set; }
         public DateTime CreatedAt { get; set; }
