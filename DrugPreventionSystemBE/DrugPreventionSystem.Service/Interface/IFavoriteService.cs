a2f1351dbd6cf0f3cbd1ac309e0c71260996b0d9
colgnaoh <lochoang2303@gmail.comgit config --global user.email lochoang2303@gmail.com>
2025-07-21 16:46:24 +0700

    add favorite interface

---
+++ DrugPreventionSystemBE/DrugPreventionSystem.Service/Interface/IFavoriteService.cs
@@ -0,0 +1,13 @@
+﻿using DrugPreventionSystemBE.DrugPreventionSystem.Enum;
+using DrugPreventionSystemBE.DrugPreventionSystem.ModelView.AddFavoriteReqModel;
+using DrugPreventionSystemBE.DrugPreventionSystem.ModelView.AddFavoriteResModel;
+
+namespace DrugPreventionSystemBE.DrugPreventionSystem.Service.Interface
+{
+    public interface IFavoriteService
+    {
+        Task<bool> AddToFavoriteAsync(Guid userId, AddFavoriteRequestModel model);
+        Task<bool> RemoveFromFavoriteAsync(Guid userId, Guid targetId, FavoriteType targetType);
+        Task<List<FavoriteItemResponseModel>> GetFavoritesAsync(Guid userId);
+    }
+}
