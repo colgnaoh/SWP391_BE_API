214c9f7dc7a5cd3b512c9a0403b34209fae50f99
colgnaoh <lochoang2303@gmail.comgit config --global user.email lochoang2303@gmail.com>
2025-07-21 16:41:43 +0700

    add favorite modelview

---
+++ DrugPreventionSystemBE/DrugPreventionSystem.ModelView/AddFavoriteReqModel/AddFavoriteRequestModel.cs
@@ -0,0 +1,10 @@
+﻿using DrugPreventionSystemBE.DrugPreventionSystem.Enum;
+
+namespace DrugPreventionSystemBE.DrugPreventionSystem.ModelView.AddFavoriteReqModel
+{
+    public class AddFavoriteRequestModel
+    {
+        public Guid TargetId { get; set; } // ProgramId or CourseId
+        public FavoriteType TargetType { get; set; }
+    }
+}
---
+++ DrugPreventionSystemBE/DrugPreventionSystem.ModelView/AddFavoriteResModel/FavoriteItemResponseModel.cs
@@ -0,0 +1,12 @@
+﻿namespace DrugPreventionSystemBE.DrugPreventionSystem.ModelView.AddFavoriteResModel
+{
+    public class FavoriteItemResponseModel
+    {
+        public Guid TargetId { get; set; }
+        public string? Name { get; set; }
+        public string? Type { get; set; } // "Course" or "Program"
+        public string? ImgUrl { get; set; }
+        public string? Description { get; set; }
+    }
+
+}
