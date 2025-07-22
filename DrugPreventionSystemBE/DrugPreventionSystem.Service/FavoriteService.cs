072ece5fd722bc9af9ebee712da59d781a91ae83
colgnaoh <lochoang2303@gmail.comgit config --global user.email lochoang2303@gmail.com>
2025-07-22 15:14:56 +0700

    implement interface

---
+++ DrugPreventionSystemBE/DrugPreventionSystem.Service/FavoriteService.cs
@@ -0,0 +1,25 @@
+﻿using DrugPreventionSystemBE.DrugPreventionSystem.Enum;
+using DrugPreventionSystemBE.DrugPreventionSystem.ModelView.AddFavoriteReqModel;
+using DrugPreventionSystemBE.DrugPreventionSystem.ModelView.AddFavoriteResModel;
+using DrugPreventionSystemBE.DrugPreventionSystem.Service.Interface;
+
+namespace DrugPreventionSystemBE.DrugPreventionSystem.Service
+{
+    public class FavoriteService : IFavoriteService
+    {
+        public Task<bool> AddToFavoriteAsync(Guid userId, AddFavoriteRequestModel model)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<FavoriteItemResponseModel>> GetFavoritesAsync(Guid userId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> RemoveFromFavoriteAsync(Guid userId, Guid targetId, FavoriteType targetType)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
